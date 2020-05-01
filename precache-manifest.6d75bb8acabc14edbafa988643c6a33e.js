772573c56e9f8f2965108926fbfd59e4eecc331c
Dmitriy Sheleg <sheleg.dmitriy@gmail.com>
2020-05-01 23:40:30 +0300

    Updates

---
+++ precache-manifest.6d75bb8acabc14edbafa988643c6a33e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "817d384f5080b8b7e2c6d5444eb18f69",
+    "url": "/test-task/index.html"
+  },
+  {
+    "revision": "f1c097ba0b463a83e946",
+    "url": "/test-task/static/css/2.f829a1d5.chunk.css"
+  },
+  {
+    "revision": "095221bdd57a5082b9e6",
+    "url": "/test-task/static/css/main.bb385104.chunk.css"
+  },
+  {
+    "revision": "f1c097ba0b463a83e946",
+    "url": "/test-task/static/js/2.5e4b260a.chunk.js"
+  },
+  {
+    "revision": "d961fa1b3613509a0e3748f5346ff00f",
+    "url": "/test-task/static/js/2.5e4b260a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "095221bdd57a5082b9e6",
+    "url": "/test-task/static/js/main.80d31208.chunk.js"
+  },
+  {
+    "revision": "edc3493cfee7352a9edc",
+    "url": "/test-task/static/js/runtime-main.47eeec77.js"
+  }
+]);
